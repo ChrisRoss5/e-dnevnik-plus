e87e6fd87800f2e27542a924ed42378e8fa70dc9
Chris <kristijan.ros@gmail.com>
2021-11-10 17:47:26 +0100

    fixed public path

--- ednevnik.plus/vue.config.js
+++ ednevnik.plus/vue.config.js
@@ -3,21 +3,19 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  publicPath: '/',
+  publicPath: "",
   css: {
     loaderOptions: {
       sass: {
         prependData: `
           @import "@/styles/global.scss";
-        `
-      }
-    }
+        `,
+      },
+    },
   },
-  chainWebpack: config =>
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].title = "e-Dnevnik Plus";
-        return args;
-      }),
-}
+  chainWebpack: (config) =>
+    config.plugin("html").tap((args) => {
+      args[0].title = "e-Dnevnik Plus";
+      return args;
+    }),
+};
