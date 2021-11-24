d9afd9208805b64a65d4c9c17759a13876ca3f42
Chris <kristijan.ros@gmail.com>
2021-11-24 04:25:23 +0100

    fixed about-app iframe

--- ednevnik.plus/src/main.ts
+++ ednevnik.plus/src/main.ts
@@ -4,13 +4,20 @@ import router from "./router";
 
 const app = createApp(App);
 app.config.globalProperties.$lastVersion = "5.0.1";
-app.config.globalProperties.$inApp =
-  window.top && window.top.window.location.protocol == "chrome-extension:";
+app.config.globalProperties.$inApp = inIframe();
 app.use(router).mount("#app");
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
-    $lastVersion: string,
-    $inApp: boolean
+    $lastVersion: string;
+    $inApp: boolean;
+  }
+}
+
+function inIframe() {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
   }
 }
