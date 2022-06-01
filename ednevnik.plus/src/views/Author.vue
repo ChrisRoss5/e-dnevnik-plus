6fbfdad1fd6b8c046473634a17aa5db626f79897
Kristijan Rosandić <kristijan.ros@gmail.com>
2022-06-01 11:32:51 +0200

    updated author

--- ednevnik.plus/src/views/Author.vue
+++ ednevnik.plus/src/views/Author.vue
@@ -17,23 +17,82 @@
     >
       članak</a
     >, a potom i nekoliko drugih portala. Proširenje dostiže 2500 tjedno
-    aktivnih korisnika pred kraj školske godine. Prelazim u ETŠ Zagreb i nastavljam
-    nadograđivati proširenje s mnoštvom novih funkcionalnosti. Od školske godine
-    2019./2020. e-Dnevnik više ne prikazuje prosjek ocjena i naglo se povećava
-    potražnja za ovim proširenjem.
-    <br /><br />
+    aktivnih korisnika pred kraj školske godine. Prelazim u ETŠ Zagreb i
+    nastavljam nadograđivati proširenje s mnoštvom novih funkcionalnosti. Od
+    školske godine 2019./2020. e-Dnevnik više ne prikazuje prosjek ocjena i
+    naglo se povećava potražnja za proširenjem. Početkom 2020. godine objavio
+    sam e-Dnevnik Plus za nastavnike i
+    <a
+      class="plus"
+      href="https://chrisross5.github.io/e-Dnevnik-Plus-WEB-stari/skole/"
+      target="_blank"
+      >e-Dnevnik Plus za škole</a
+    >
+    (verzija za info-stupove).<br /><br />
     Ovako je e-Dnevnik Plus izgledao školske godine 2019./2020.:
     <img src="~@/assets/img/history/v2.png" />
     Pred početak školske godine 2020./2021., e-Dnevnik je redizajniran s novim
     funkcionalnostima. To znači da sam trebao izraditi gotovo sve ispočetka.
-    <br /><br />
+    <a
+      class="plus"
+      href="https://www.youtube.com/watch?v=t_lQPHSU8T4&list=PLzgE2RyhiYODJlMcdx7AzpmclEBe0Dbeo&ab_channel=KristijanRoss"
+      target="_blank"
+    >
+      Playlista na Youtubeu</a
+    >
+    sadrži obilaske kroz 4 najbitnije stare verzije proširenja. Stara naslovna
+    stranica (s verzijama &lt; 5.0) nalazi se
+    <a
+      class="plus"
+      href="https://chrisross5.github.io/e-Dnevnik-Plus-WEB-stari/"
+      target="_blank"
+    >
+      ovdje</a
+    >.<br /><br />
     Ovako je e-Dnevnik Plus izgledao školske godine 2020./2021.:
     <img src="~@/assets/img/history/v3.png" />
-    Ipak, godinu dana kasnije (ljeto 2021. nakon mature) odlučujem opet
-    izbrisati sve i započeti iznova. Ali ovaj put na pravi način. Krpanje
-    "početničkog" koda sa starim tehnologijama nije bilo zabavno. Stoga, danas
-    postoji moja verzija e-Dnevnika - Plus App, ali i jednostavna i sigurna Classic
-    verzija koja se ugrađuje u e-Dnevnik kao što je bilo i prije.
+    e-Dnevnik Plus pobjeđuje na
+    <a
+      class="plus"
+      href="https://informatika.azoo.hr/natjecanje/dogadjaj/614/rezultati"
+      target="_blank"
+    >
+      državnom natjecanju iz informatike
+    </a>
+    te je izabran kao najbolji rad na izložbi
+    <a
+      class="plus"
+      href="https://inova-croatia.com/en/product-detail/e-dnevnik-plus-2/"
+      target="_blank"
+    >
+      INOVA</a
+    >. Ipak, godinu dana kasnije (ljeto 2021. nakon mature) odlučujem opet
+    izbrisati sve i započeti iznova, ali ovaj put na pravi način. Krpanje
+    početničkog koda sa starim tehnologijama nije bilo zabavno. Stoga, danas
+    postoji moja moderna verzija e-Dnevnika - Plus App, ali i jednostavna i
+    sigurna Classic verzija koja se ugrađuje u e-Dnevnik kao što je bilo i
+    prije.
+    <br />
+    <br />
+    <hr />
+    <br />
+    e-Dnevnik Plus 5.0 je Open Source, izrađen u Vue Frameworku s Typescriptom.
+    Razvoj je trajao 5 mjeseci bez razmaka, što je otprilike 1000 sati rada. Za
+    donacije, klikni
+    <a class="plus" href="https://ednevnik.plus/donacije" target="_blank">
+      ovdje</a
+    >. Za pregled drugih aplikacija i proširenja, vidi moj Developer Portfolio
+    na
+    <a class="plus" href="https://kristijanros.dev" target="_blank">
+      kristijanros.dev</a
+    > (dolazi uskoro).
+    <br />
+    <br />
+    <br />
+    <br />
+    <div style="color: gray; font-style: italic; text-align: right">
+      — Kristijan Rosandić, Visoko učilište Algebra
+    </div>
   </div>
 </template>
 
